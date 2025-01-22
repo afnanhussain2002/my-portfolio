c9b4505d94e74fb629563f2c18af04a271e6628d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-22 12:56:45 +0600

    add some responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const MainPage = () => {
   return (
-    <main className='flex'>
+    <main className='flex flex-col md:flex-row'>
       <section className='w-72 h-screen bg-[url("../public/images/mainBg.jpg")] bg-cover'>
        <Profile />
       </section>
       <section className='mx-auto'>
 
-        <div className=' w-[700px] h-80 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5'>
+        <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5'>
           Main section
         </div>
 
