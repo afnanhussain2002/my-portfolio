8aabe29ae86d016908ecb74e3fd3170d831d47a3
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 12:01:38 +0600

    remove image import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,12 @@
-import Image from 'next/image'
 import React from 'react'
-import linkdin from "../public/images/linkdin.webp";
-import facebook from "../public/images/facebook.png";
-import github from "../public/images/github.png";
-import x from "../public/images/x.png";
+
 
 
 const Footer = () => {
   return (
     <div>
-        <div>
-           
+        <div className='flex gap-5'>
+            
         </div>
     </div>
   )
