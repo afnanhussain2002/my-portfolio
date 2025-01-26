140411395873fc70f1e29041badfff2da8261c05
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 11:58:39 +0600

    import all social images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 import linkdin from "../public/images/linkdin.webp";
+import facebook from "../public/images/facebook.png";
+import github from "../public/images/github.png";
+import x from "../public/images/x.png";
 
 
 const Footer = () => {
