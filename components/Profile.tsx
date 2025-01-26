85af61b2890e6bec5b444509e62ad6c95ed8414d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 11:25:35 +0600

    align some formats on that code

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import profile from '../public/images/profile.png'
-import Image from 'next/image'
+import React from "react";
+import profile from "../public/images/profile.png";
+import Image from "next/image";
 
 const Profile = () => {
   return (
-    <div className='flex flex-col items-center space-y-2 mt-3'>
-      <Image src={profile} alt="profile" width={200} height={200} className='rounded-full w-40 h-40'/>
-      <h3 className='font-bold'>Afnan Hussain</h3>
+    <div className="flex flex-col items-center space-y-2 mt-3">
+      <Image
+        src={profile}
+        alt="profile"
+        width={200}
+        height={200}
+        className="rounded-full w-40 h-40"
+      />
+      <h3 className="font-bold">Afnan Hussain</h3>
       <p>Full Stack Developer</p>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
