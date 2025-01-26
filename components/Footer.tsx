cbebabdf8aea965410c03b284c29c844fd61e950
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 12:08:54 +0600

    use the logo and images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Linkedin, Github, Facebook } from 'lucide-react';
 import x from "../public/images/x.png";
+import Image from 'next/image';
 
 
 const Footer = () => {
   return (
     <div>
         <div className='flex gap-5'>
-            
+            <Linkedin size={30} />
+            <Github size={30} />
+            <Facebook size={30} />
+            <Image src={x} alt='x' width={30} height={30} />
         </div>
     </div>
   )
