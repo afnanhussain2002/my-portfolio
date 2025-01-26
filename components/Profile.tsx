b73fa6418cd06e80e87b05504b43ab92aeafa1b2
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 11:20:57 +0600

    add a profile with name

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 const Profile = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center space-y-2 mt-3'>
       <Image src={profile} alt="profile" width={200} height={200} className='rounded-full w-40 h-40'/>
-      <h3>Afnan Hussain</h3>
+      <h3 className='font-bold'>Afnan Hussain</h3>
       <p>Full Stack Developer</p>
     </div>
   )
