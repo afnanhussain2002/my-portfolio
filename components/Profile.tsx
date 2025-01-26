2da2e0a8ef392bdd3d279a894a53110fc21f770e
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-26 11:18:17 +0600

    add some p tags with my identity

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,6 +6,8 @@ const Profile = () => {
   return (
     <div>
       <Image src={profile} alt="profile" width={200} height={200} className='rounded-full w-40 h-40'/>
+      <h3>Afnan Hussain</h3>
+      <p>Full Stack Developer</p>
     </div>
   )
 }
