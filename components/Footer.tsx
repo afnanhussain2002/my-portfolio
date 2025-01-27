629d59d3c23cd78e5e0744c930eef4514b56b88d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 07:28:39 +0600

    add profile link on linkedin icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,10 @@ const Footer = () => {
   return (
     <div className='flex justify-center'>
         <div className='flex gap-5 bg-[#076871] p-2 w-52 h-10 rounded items-center justify-center'>
+          <a href="https://www.linkedin.com/in/md-afnan-hussain/" target='_blank'>
+          
             <Linkedin size={30} className='bg-black p-1 rounded'/>
+          </a>
             <Github size={30} className='bg-black p-1 rounded'/>
             <Facebook size={30} className='bg-black p-1 rounded'/>
             <Image src={x} alt='x' width={30} height={30} />
