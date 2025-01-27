9a87fa19881c61f8f59841fc8fdeb1ab287538d4
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 07:48:16 +0600

    add  main text

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5'>
-          Main section
+          <div>
+            <h2>Fresher Full Stack Developer</h2>
+          </div>
+          <div>
+            images
+          </div>
         </div>
   )
 }
