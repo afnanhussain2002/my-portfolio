21a24d930f6a64027e46534595192c2a200226a6
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 09:56:09 +0600

    add some re useable code

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,27 +1,24 @@
 import React from 'react'
+import ShimmerBg from './ShimmerBg'
 
 
 const TechStack = () => {
   return (
     <div className='bg-white w-96 h-96 text-black flex justify-center items-center'>
      <div className=' w-full h-full p-5 flex flex-col justify-center items-center space-y-1'>
-        <p className='font-bold bg-[#076871] p-2 text-white w-28 h-10 text-center inline-flex animate-shimmer items-center justify-center rounded-md border bg-[linear-gradient(110deg,#319795,45%,#00F5F9,55%,#319795)] bg-[length:200%_100%] px-6 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>MongoDB</p>
+        <ShimmerBg text='MongoDB'/>
         <div className='w-full flex justify-around'>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>NodeJS</p>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>ExpressJs</p>
+        <ShimmerBg text='NodeJS'/>
+        <ShimmerBg text='ExpressJS'/>
         </div>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>JavaScript</p>
+        <ShimmerBg text='JavaScript'/>
         <div className='w-full flex justify-around'>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>ReactJs</p>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>NextJs</p>
+        <ShimmerBg text='ReactJs'/>
+        <ShimmerBg text='NextJs'/>
         </div>
-        <p className='font-bold bg-[#076871] p-2 rounded text-white w-28 h-10 text-center'>TypeScript</p>
+       <ShimmerBg text='TypeScript'/>
 
      </div>
-    
-   {/*  <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border bg-teal-600 bg-[linear-gradient(110deg,#319795,45%,#00F5F9,55%,#319795)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-  Shimmer
-</button> */}
 </div>
 
  
