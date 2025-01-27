84ed24da615aa068f8aafcc3bd75e23c37bad820
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 11:01:49 +0600

    add default options for lottie

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,10 +1,20 @@
 import { Copy } from 'lucide-react'
 import React, { useState } from 'react'
 
+
 const CopyButton = () => {
     const [copied, setCopied] = useState(false);
 
-    
+    const defaultOptions = {
+        loop: copied,
+        autoplay: copied,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      };
+
+
   return (
     <div>
         <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
