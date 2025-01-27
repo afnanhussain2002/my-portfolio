d1a5da0c6d917b77d2cd3bfa108f9944e26b9d67
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 09:58:36 +0600

    change some design of css

--- components/ShimmerBg.tsx
+++ components/ShimmerBg.tsx
@@ -5,7 +5,7 @@ interface ShimmerBgProps {
   }
 const ShimmerBg: React.FC<ShimmerBgProps> = ({text}) => {
   return (
-    <div className='font-bold bg-[#076871] p-2 text-white w-28 h-10 text-center inline-flex animate-shimmer items-center justify-center rounded-md border bg-[linear-gradient(110deg,#319795,45%,#00F5F9,55%,#319795)] bg-[length:200%_100%] px-6 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+    <div className='font-bold bg-[#076871] p-2 text-white w-28 h-10 text-center border-none inline-flex animate-shimmer items-center justify-center rounded-md border bg-[linear-gradient(110deg,#319795,45%,#00F5F9,55%,#319795)] bg-[length:200%_100%] px-6 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
      {text}
     </div>
   )
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -4,7 +4,7 @@ import ShimmerBg from './ShimmerBg'
 
 const TechStack = () => {
   return (
-    <div className='bg-white w-96 h-96 text-black flex justify-center items-center'>
+    <div className='w-96 h-96  flex justify-center items-center'>
      <div className=' w-full h-full p-5 flex flex-col justify-center items-center space-y-1'>
         <ShimmerBg text='MongoDB'/>
         <div className='w-full flex justify-around'>
