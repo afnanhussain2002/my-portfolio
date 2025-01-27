0238a8c610a597364da70b9ace334c70eff79660
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 10:08:06 +0600

    change some padding

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex justify-around items-center p-5'>
           <div className='ml-10'>
             <h2 className='text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
-            <button className='mt-5 bg-[#076871] p-3 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
+            <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
           </div>
           <div>
             <TechStack/>
