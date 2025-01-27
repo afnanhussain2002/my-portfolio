7f8a9a97c7d2c96b1aafefef81382303691d019c
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 10:06:40 +0600

    add icon on copy button

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import TechStack from './TechStack'
+import { Copy } from 'lucide-react';
 
 const HeroSection = () => {
   return (
     <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex justify-around items-center p-5'>
           <div className='ml-10'>
             <h2 className='text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
-            <button className='mt-5 bg-[#076871] p-2 rounded font-bold'>Copy My Email</button>
+            <button className='mt-5 bg-[#076871] p-3 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
           </div>
           <div>
             <TechStack/>
