3d69e5d5eb86ed5f41985bf9993f7a4e3a120536
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 10:59:50 +0600

    add a state for copied

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -2,6 +2,7 @@ import { Copy } from 'lucide-react'
 import React from 'react'
 
 const CopyButton = () => {
+    const [copied, setCopied] = useState(false);
   return (
     <div>
         <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
