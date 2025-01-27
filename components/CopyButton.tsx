f2b976ebac25e5c323add96c4eafc7c7c304767f
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 11:01:03 +0600

    install lottie

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 
 const CopyButton = () => {
     const [copied, setCopied] = useState(false);
+
+    
   return (
     <div>
         <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
