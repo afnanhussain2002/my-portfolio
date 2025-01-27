5d77199b5c4f454290eaacfd7f90512c95b874ca
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 07:52:26 +0600

    add buttons with some design

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5'>
-          <div>
-            <h2>Fresher Full Stack Developer</h2>
+    <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex justify-between items-center p-5'>
+          <div className='ml-10'>
+            <h2 className='text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
+            <button className='mt-5 bg-[#076871] p-2 rounded font-bold'>Copy My Email</button>
           </div>
           <div>
             images
