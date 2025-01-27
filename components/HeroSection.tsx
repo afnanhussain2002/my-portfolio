16fa537de6f079229c880abb5353c974c6e9212a
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 11:09:27 +0600

    import copy button

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import TechStack from './TechStack'
 import { Copy } from 'lucide-react';
+import CopyButton from './CopyButton';
 
 const HeroSection = () => {
   return (
     <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex justify-around items-center p-5'>
           <div className='ml-10'>
             <h2 className='text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
-            <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
+           <CopyButton/>
           </div>
           <div>
             <TechStack/>
