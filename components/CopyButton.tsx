e3d63df3beaeb83e8c902737692f078ce9497f20
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 10:59:06 +0600

    add the copy button

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CopyButton = () => {
   return (
     <div>
-        
+        <button className='mt-5 bg-[#076871] px-3 py-2 rounded font-bold flex gap-2 items-center'><Copy/> Copy My Email</button>
     </div>
   )
 }
