5b1a2a147bd4207788af007a97ea13631dd9226e
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-27 09:52:38 +0600

    define the props for text

--- components/ShimmerBg.tsx
+++ components/ShimmerBg.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const ShimmerBg = () => {
+interface ShimmerBgProps {
+    text: string;
+  }
+const ShimmerBg: React.FC<ShimmerBgProps> = ({text}) => {
   return (
-    <div>
-        
+    <div className='font-bold bg-[#076871] p-2 text-white w-28 h-10 text-center inline-flex animate-shimmer items-center justify-center rounded-md border bg-[linear-gradient(110deg,#319795,45%,#00F5F9,55%,#319795)] bg-[length:200%_100%] px-6 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+     {text}
     </div>
   )
 }
