babaded256990b432234d5c2293fa1cb61425dca
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-29 19:33:56 +0600

    add a portfolio data

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,12 @@
+
+
 export const projects = [
-    
+    {
+        id: 1,
+        title:"Making Chip - AI Image Tools",
+        des:"Boost Your Website's Performance with using our tools. You can optimize, format, and restore your images with just a few clicks.",
+        img:"/images/makingchip.png",
+        iconLists:["/public/images/next.svg", "/public/images/ts.svg", "/public/images/tail.svg","/public/images/cloudinary.png"],
+        link:"https://www.makingchip.com"
+    }
 ]
