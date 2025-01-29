6ce96c9efd01d00967fdb80f7e6fcd1fcf8f750b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-29 18:43:15 +0600

    add some design on that text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div>
-        <h3>selection of recent projects</h3>
+        <h3 className='text-3xl font-bold'>Selection of recent projects</h3>
     </div>
   )
 }
