78939de4369533b040e8e243db1fa118afd247f8
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-29 18:44:03 +0600

    add some margin and center the text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
+    <div className='mt-5 text-center'>
         <h3 className='text-3xl font-bold'>Selection of recent projects</h3>
     </div>
   )
