38417345c86ec5a6026da5705331ba3ca7c5ec74
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-29 19:41:08 +0600

    add the second data

--- data/index.ts
+++ data/index.ts
@@ -9,5 +9,13 @@ export const projects = [
         iconLists:["/public/images/next.svg", "/public/images/ts.svg", "/public/images/tail.svg","/public/images/cloudinary.png"],
         link:"https://www.makingchip.com"
     },
-
+    {
+        id: 2,
+        title:"Client Echo - Where your identity remains a secret.",
+        des:"Clientecho is a platform designed to allow users to provide anonymous feedback, creating an open and safe space for clients to express their thoughts freely.",
+        img:"/public/images/Portfolio2.png",
+        iconLists:["/public/images/next.svg", "/public/images/ts.svg", "/public/images/tail.svg","/public/images/mongoDB.svg"],
+        link:"https://clientecho.vercel.app/"
+    },
+    
 ]
