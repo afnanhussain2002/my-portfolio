11d4e234e5c01d558b467781551699679c0f3324
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-30 19:11:02 +0600

    delete recent project code

--- data/index.ts
+++ data/index.ts
@@ -5,24 +5,24 @@ export const projects = [
         id: 1,
         title:"Making Chip - AI Image Tools",
         des:"Boost Your Website's Performance with using our tools. You can optimize, format, and restore your images with just a few clicks.",
-        img:"/public/images/makingchip.png",
-        iconLists:["/public/images/next.svg", "/public/images/ts.svg", "/public/images/tail.svg","/public/images/cloudinary.png"],
+        img:"/images/makingchip.png",
+        iconLists:["/images/next.svg", "/images/ts.svg", "/images/tail.svg","/images/cloudinary.png"],
         link:"https://www.makingchip.com"
     },
     {
         id: 2,
         title:"Client Echo - Where your identity remains a secret.",
         des:"Clientecho is a platform designed to allow users to provide anonymous feedback, creating an open and safe space for clients to express their thoughts freely.",
-        img:"/public/images/Portfolio2.png",
-        iconLists:["/public/images/next.svg", "/public/images/ts.svg", "/public/images/tail.svg","/public/images/mongoDB.svg"],
+        img:"/images/Portfolio2.png",
+        iconLists:["/images/next.svg", "/images/ts.svg", "/images/tail.svg","/images/mongoDB.svg"],
         link:"https://clientecho.vercel.app/"
     },
     {
         id: 3,
         title:"PlayPlex",
         des:"When i was new in Mern Stack, I build that website and practice some design and make a grate UI for it.",
-        img:"/public/images/Portfolio3.png",
-        iconLists:["/public/images/re.svg", "/public/images/js.webp", "/public/images/nodeJs.svg", "/public/images/tail.svg","/public/images/mongoDB.svg"],
+        img:"/images/Portfolio3.png",
+        iconLists:["/images/re.svg", "/images/js.webp", "/images/nodeJs.svg", "/images/tail.svg","/images/mongoDB.svg"],
         link:"https://play-plex-bf61f.web.app/"
     },
     
