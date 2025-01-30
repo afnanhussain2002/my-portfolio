276c34615854849f9eb8bc6a9734c9b41a4c509b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-30 19:30:41 +0600

    change the size of title

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
      className=" flex flex-col items-center justify-center w-96"
      key={project.id}
      >
-    <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+    <div className="relative flex items-center justify-center w-96  overflow-hidden h-[20vh] lg:h-[30vh] ">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
@@ -24,7 +24,7 @@ const Projects = () => {
                   className="z-10 absolute bottom-0"
                 />
               </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+              <h1 className="font-bold text-base line-clamp-1">
                 {project.title}
               </h1>
      </div>
