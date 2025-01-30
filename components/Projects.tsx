cdaa3205609b0ab810621c82ab2b9dace4f863e6
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-30 19:13:46 +0600

    write the first div for show the project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,16 @@ const Projects = () => {
   return (
     <div className='mt-5 text-center'>
         <h3 className='text-3xl font-bold'>Selection of recent projects</h3>
+<div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+   {projects.map((project) => (
+     <div
+     className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+     key={project.id}
+     >
 
+     </div>
+   ))}
+</div>
     </div>
   )
 }
