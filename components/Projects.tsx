4ece77948c7cf41e03c1ed21cb1b00059a28d8be
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-01-30 20:03:34 +0600

    add icons and add border

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { MoveUpRight } from 'lucide-react';
 
 const Projects = () => {
   return (
@@ -8,7 +9,7 @@ const Projects = () => {
 <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
    {projects.map((project) => (
      <div
-     className=" flex flex-col items-center justify-center text-left w-96"
+     className=" flex flex-col items-center justify-center text-left w-96 border"
      key={project.id}
      >
     <div className="relative flex items-center justify-center w-96  overflow-hidden h-[20vh] lg:h-[30vh] ">
@@ -35,8 +36,24 @@ const Projects = () => {
               </p>
             <div className='flex items-center justify-between mt-7 mb-3'>
          <div className='flex items-center'>
-
+       {project.iconLists.map((icon, index) => (
+         <div
+         key={index}
+         className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+         style={{
+          transform: `translateX(-${5 * index + 2}px)`,
+        }}
+       >
+         <img src={icon} alt="icon5" className="p-2" />
+       </div>
+       ))}
          </div>
+         <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <MoveUpRight className="ms-3" color="#CBACF9" />
+                </div>
             </div>
      </div>
    ))}
