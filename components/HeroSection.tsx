f847ab6f794bdd4e0bde4e3263dc152f23b3bdd7
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-02 22:19:36 +0600

    hidden the main hero text for mobile

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,8 +5,8 @@ import CopyButton from './CopyButton';
 const HeroSection = () => {
   return (
     <div className='sm:w-96 sm:h-32 lg:w-[1280px] lg:h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex flex-col-reverse justify-around items-center p-5 lg:flex-row'>
-          <div className='ml-10'>
-            <h2 className='sm:text-4xl lg:text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
+          <div className='lg:ml-10'>
+            <h2 className='hidden text-5xl font-bold lg:flex'>Fresher Full Stack <br /> Developer</h2>
            <CopyButton/>
           </div>
           <div>
