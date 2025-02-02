b9e3fa53062b9ed6c0382d364d323d9b120cb1cf
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-02 21:29:38 +0600

    write code for toggle

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,33 @@ const MainPage = () => {
   return (
     <>
     
-    <main className='flex flex-col md:flex-row'>
-      <section className='w-72 h-screen bg-[url("../public/images/mainBg.jpg")] bg-cover flex flex-col justify-between p-2'>
-    
-       <ProfileCard />
-       <AboutMe/>
-       <div className='mb-5'>
-       <Footer/>
-       </div>
-      </section>
-      <section className='mx-auto'>
+    <main className="flex flex-col md:flex-row">
+      {/* Toggle Button (Only on Mobile) */}
+      <button
+        className="md:hidden p-2 bg-gray-800 text-white rounded-md mx-4 mt-2"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? "Close" : "Menu"}
+      </button>
 
-       <HeroSection/>
-       <Projects/>
+      {/* Sidebar Section */}
+      <section
+        className={`w-72 h-screen bg-[url("../public/images/mainBg.jpg")] bg-cover flex flex-col justify-between p-2 absolute md:relative ${
+          isOpen ? "block" : "hidden"
+        } md:block`}
+      >
+        <ProfileCard />
+        <AboutMe />
+        <div className="mb-5">
+          <Footer />
+        </div>
       </section>
 
+      {/* Main Content */}
+      <section className="mx-auto">
+        <HeroSection />
+        <Projects />
+      </section>
     </main>
     </>
   )
