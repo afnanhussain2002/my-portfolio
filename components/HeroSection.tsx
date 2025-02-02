37eb2abb1d5778d513f5a3795b3ee9131f22aace
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-02 21:18:34 +0600

    add some more responsive css on hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className='sm:w-96 sm:h-32 lg:w-[1280px] lg:h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex flex-col-reverse justify-around items-center p-5 lg:flex-row'>
           <div className='ml-10'>
-            <h2 className='sm:text-3xl lg:text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
+            <h2 className='sm:text-4xl lg:text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
            <CopyButton/>
           </div>
           <div>
