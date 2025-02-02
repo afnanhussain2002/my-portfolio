000223a62b4dba9920b94e6a69c2fb59df1288ee
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-02 21:10:05 +0600

    add some responsive on hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,9 +4,9 @@ import CopyButton from './CopyButton';
 
 const HeroSection = () => {
   return (
-    <div className=' w-[1280px] h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex justify-around items-center p-5'>
+    <div className='sm:w-96 sm:h-32 lg:w-[1280px] lg:h-96 bg-[url("../public/images/mainBg.jpg")] bg-cover mt-5 flex flex-col-reverse justify-around items-center p-5 lg:flex-row'>
           <div className='ml-10'>
-            <h2 className='text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
+            <h2 className='sm:text-3xl lg:text-5xl font-bold '>Fresher Full Stack <br /> Developer</h2>
            <CopyButton/>
           </div>
           <div>
