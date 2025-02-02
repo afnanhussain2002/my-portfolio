7467cc08c886908925c4ea8183653836ad9cc99b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-02 21:44:32 +0600

    get the previous code

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,30 @@
-"use client";
 import AboutMe from '@/components/AboutMe'
 import Footer from '@/components/Footer'
 import HeroSection from '@/components/HeroSection'
 import ProfileCard from '@/components/ProfileCard'
 import Projects from '@/components/Projects'
-import React, { useState } from 'react'
+import React from 'react'
 
 
 const MainPage = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
     
-    <main className="flex flex-col md:flex-row">
-      {/* Toggle Button (Only on Mobile) */}
-      <button
-        className="md:hidden p-2 bg-gray-800 text-white rounded-md mx-4 mt-2"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {isOpen ? "Close" : "Menu"}
-      </button>
+    <main className='flex flex-col md:flex-row'>
+      <section className='w-72 h-screen bg-[url("../public/images/mainBg.jpg")] bg-cover flex flex-col justify-between p-2'>
+    
+       <ProfileCard />
+       <AboutMe/>
+       <div className='mb-5'>
+       <Footer/>
+       </div>
+      </section>
+      <section className='mx-auto'>
 
-      {/* Sidebar Section */}
-      <section
-        className={`w-72 h-screen bg-[url("../public/images/mainBg.jpg")] bg-cover flex flex-col justify-between p-2 absolute md:relative ${
-          isOpen ? "block" : "hidden"
-        } md:block`}
-      >
-        <ProfileCard />
-        <AboutMe />
-        <div className="mb-5">
-          <Footer />
-        </div>
+       <HeroSection/>
+       <Projects/>
       </section>
 
-      {/* Main Content */}
-      <section className="mx-auto">
-        <HeroSection />
-        <Projects />
-      </section>
     </main>
     </>
   )
